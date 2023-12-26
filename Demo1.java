0212533c9ae8e7e83e15903eccb9ca785b670184
damu399 <kdamodhar399@gmail.com>
2023-12-26 20:37:41 +0530

    a1

--- Demo1.java
+++ Demo1.java
@@ -2,6 +2,8 @@ public class Demo1 {
     public static void main(String[] args) {
         
         String yourName = "damodhar";
+	String yourName1 = "adi lakshmi";
         System.out.println("Hello, " + yourName + "!");
+	System.out.println("Hello, " + yourName1 + "!");
     }
 }
